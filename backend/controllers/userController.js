3cc71f4dcd194617e4c78b96be7baf4618fc5ac6
Jared Silver <jsilver@Jareds-MBP-2.lan>
2023-12-26 14:18:46 -0800

    user authentication, login, logout, access cookie securely, and auth middleware for protected routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,109 @@
+import asyncHandler from "../middleware/asyncHandler.js"
+import User from "../models/userModel.js"
+import jwt from "jsonwebtoken"
+
+// @desc Auth user & get token
+// @route POST /api/users/login
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    })
+
+    // Set JWT as http-only cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 Days
+    })
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(401)
+    throw new Error("Invalid email or password")
+  }
+})
+
+// @desc Register User
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.send("Register user")
+})
+
+// @desc Logout User & clear cookie
+// @route POST /api/users/logout
+// @access Private
+const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  })
+
+  res.status(200).json({ msg: "Logged out successfully" })
+})
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send("Get user profile")
+})
+
+// @desc Update user profile
+// @route PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("Update user profile")
+})
+
+// @desc Get users
+// @route GET /api/users
+// @access Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("Get users")
+})
+
+// @desc Delete user
+// @route DELETE /api/users/:id
+// @access Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("Delete user")
+})
+
+// @desc Get user by ID
+// @route GET /api/users/:id
+// @access Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("Get user by Id")
+})
+
+// @desc Update User
+// @route PUT /api/users/:id
+// @access Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("Update user")
+})
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  getUserById,
+  getUsers,
+  updateUserProfile,
+  updateUser,
+  deleteUser,
+}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import connectDB from "./config/db.js"
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js"
 dotenv.config()
 
@@ -11,12 +13,19 @@ connectDB()
 
 const app = express()
 
+// Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser())
+
 app.get("/", (req, res) => {
   res.send("API Running...")
 })
 
 // API Routes
 app.use("/api/products", productRoutes)
+app.use("/api/users", userRoutes)
 
 // Error Middleware
 app.use(notFound)
