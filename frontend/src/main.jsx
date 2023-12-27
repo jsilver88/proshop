98e2f6a318c82ddf2ded3add803180f482632b6d
Jared Silver <jsilver@Jareds-MacBook-Pro-2.local>
2023-12-27 11:22:57 -0800

    User auth frontend, adding routes to redux toolkit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,8 @@ import App from "./App.jsx"
 import HomeScreen from "./screens/HomeScreen.jsx"
 import ProductScreen from "./screens/ProductScreen.jsx"
 import CartScreen from "./screens/CartScreen.jsx"
+import LoginScreen from "./screens/LoginScreen.jsx"
+import RegisterScreen from "./screens/RegisterScreen.jsx"
 import "./assets/styles/bootstrap.custom.css"
 import "./index.css"
 
@@ -21,6 +23,8 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
 )
